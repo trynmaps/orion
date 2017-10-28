3bb82d227b17d6c218aac7587b9798e202aa0ca9
eddyionescu <eddyionescu@gmail.com>
2017-10-27 23:59:03 -0700

    update logging and console outputs

--- cassandraHelper.js
+++ cassandraHelper.js
@@ -5,12 +5,15 @@ const client = new cassandra.Client({
     contactPoints: [config.cassandraURL],
 });
 client.connect((err) => {
-    console.log(err);
+    console.log('connected to cassandra');
+    console.log('error:' + err);
 });
 
 function cassandraBatch(queries) {
     client.batch(queries, { prepare: true }, (err) => {
-        console.log(err);
+        if (err) {
+            console.log('error:' + err);
+        }
         console.log('Data updated on cluster');
     });
 }
