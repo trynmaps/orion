d330e042867df075df62d74c1bc17c6a35409f61
eddyionescu <eddyionescu@gmail.com>
2018-01-15 01:42:02 -0500

    7 - Add TTC
    This commit adds TTC support to Orion by having it
    reuse the existing functions that were already
    being used for Muni.
    
    https://github.com/trynmaps/orion/issues/7
    
    The next step is to generalize these functions into
    a Nextbus module so that all agencies using nextbus
    can automatically be added into OpenTransit.
    
    TODO - https://github.com/trynmaps/orion/issues/8

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const config = require('./config');
 
 const muni = require('./agencies/muni/muni');
 const marin = require('./agencies/marin/marin');
+const ttc = require('./agencies/ttc/ttc');
 
 setInterval(updateVehicles, 15000);
 
 function updateVehicles() {
-  return Promise.all([muni, marin].map((agency) => {
+  return Promise.all([muni, marin, ttc].map((agency) => {
     return agency();
   })).catch((err) => {
     console.log(err);
