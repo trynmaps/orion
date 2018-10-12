bb44960494e38573e14a9daac85c0c8df3fd553b
eddyionescu <eddyionescu@gmail.com>
2018-10-12 16:31:00 -0400

    Add gzip encoding and commit s3 upload changes

--- s3Helper.js
+++ s3Helper.js
@@ -1,8 +1,16 @@
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+var zlib = require('zlib');
 
 const config = require('./config');
+
+const s3 = new AWS.S3();
 AWS.config.loadFromPath('./prod-s3.json');
+
+function compressData(data) {
+  return new Promise((resolve, _) => {
+    return zlib.gzip(JSON.stringify(data), (_, encoded) => resolve(encoded));
+  });
+}
 
 function writeToS3(agency, currentTime, data, isRaw) {
   // TODO - in the future, gzip isRaw files
@@ -13,15 +21,19 @@ function writeToS3(agency, currentTime, data, isRaw) {
   const hour = currentDateTime.getUTCHours();
   const minute = currentDateTime.getUTCMinutes();
   const second = currentDateTime.getUTCSeconds();
-  return new Promise((resolve, reject) => {
-    s3.putObject({
-      Bucket: `${isRaw ? "orion-raw" : "orion-vehicles"}${config.dev ? "-dev" : ""}`,
-      Key: `${agency}/${year}/${month}/${day}/${hour}/${minute}/${second}/${agency}-${currentTime}.json`,
-      Body: JSON.stringify(data), 
-      ContentType: "application/json",
-    }, (err, res) => {
-      if (err) reject(err);
-      resolve(res);
+  return compressData(data).then(encodedData => {
+    return new Promise((resolve, reject) => {
+      s3.putObject({
+        Bucket: `${isRaw ? "orion-raw" : "orion-vehicles"}${config.dev ? "-dev" : ""}`,
+        Key: `${agency}/${year}/${month}/${day}/${hour}/${minute}` + 
+          `/${second}/${agency}-${currentTime}.json.gz`,
+        Body: encodedData,
+        ContentType: "text/plain",
+        ContentEncoding: "gzip",
+      }, (err, res) => {
+        if (err) reject(err);
+        resolve(res);
+      });
     });
   });
 };
